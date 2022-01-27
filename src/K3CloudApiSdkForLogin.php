fe11ed19e7dd213f6c25fc9374996858fe481756
zhangpeng <964750656@qq.com>
2022-01-27 10:58:21 +0800

    增加操作API

--- src/K3CloudApiSdkForLogin.php
+++ src/K3CloudApiSdkForLogin.php
@@ -51,9 +51,10 @@ class K3CloudApiSdkForLogin
      * 详情
      * @param $formId
      * @param $data
+     * @param string $format
      * @return mixed|string|void
      */
-    public function view($formId, $data, $format = 'string')
+    public function view($formId, $data, string $format = 'string')
     {
         $url = $this->cloudUrl . ApiPathConst::VIEW_API;
         $postData = [
@@ -66,9 +67,10 @@ class K3CloudApiSdkForLogin
     /**
      *  单据查询
      * @param $data
+     * @param string $format
      * @return mixed|string|void
      */
-    public function executeBillQuery($data, $format = 'string')
+    public function executeBillQuery($data, string $format = 'string')
     {
         $url = $this->cloudUrl . ApiPathConst::EXECUTEBILLQUERY_API;
         $postData = [
@@ -80,9 +82,10 @@ class K3CloudApiSdkForLogin
     /**
      * 元数据查询
      * @param $data
+     * @param string $format
      * @return mixed|string|void
      */
-    public function queryBusinessInfo($data, $format = 'string')
+    public function queryBusinessInfo($data, string $format = 'string')
     {
         $url = $this->cloudUrl . ApiPathConst::QUERYBUSINESSINFO_API;
         $postData = [
@@ -91,4 +94,321 @@ class K3CloudApiSdkForLogin
         return $this->webApiClient->execute($url, [], $postData, $format);
     }
 
+
+    /**
+     * 保存
+     * @param $formId
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function save($formId, $data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::SAVE_API;
+        $postData = [
+            'data' => [
+                'formid' => $formId,
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 批量保存
+     * @param $formId
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function batchSave($formId, $data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::BATCHSAVE_API;
+        $postData = [
+            'data' => [
+                'formid' => $formId,
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 审核
+     * @param $formId
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function audit($formId, $data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::AUDIT_API;
+        $postData = [
+            'data' => [
+                'formid' => $formId,
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 反审核
+     * @param $formId
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function unAudit($formId, $data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::UNAUDIT_API;
+        $postData = [
+            'data' => [
+                'formid' => $formId,
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 提交
+     * @param $formId
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function submit($formId, $data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::SUBMIT_API;
+        $postData = [
+            'data' => [
+                'formid' => $formId,
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 操作
+     * @param $formId
+     * @param $opNumber
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function operation($formId, $opNumber, $data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::EXCUTEOPERATION_API;
+        $postData = [
+            'data' => [
+                'formid' => $formId,
+                'opNumber' => $opNumber,
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 下推
+     * @param $formId
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function push($formId, $data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::PUSH_API;
+        $postData = [
+            'data' => [
+                'formid' => $formId,
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 暂存
+     * @param $formId
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function draft($formId, $data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::DRAFT_API;
+        $postData = [
+            'data' => [
+                'formid' => $formId,
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 删除
+     * @param $formId
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function delete($formId, $data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::DELETE_API;
+        $postData = [
+            'data' => [
+                'formid' => $formId,
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 分配
+     * @param $formId
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function allocate($formId, $data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::ALLOCATE_API;
+        $postData = [
+            'data' => [
+                'formid' => $formId,
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 弹性域保存
+     * @param $formId
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function flexSave($formId, $data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::FLEXSAVE_API;
+        $postData = [
+            'data' => [
+                'formid' => $formId,
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 发送消息
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function sendMsg($data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::SENDMSG_API;
+        $postData = [
+            'data' => [
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 分组保存
+     * @param $formId
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function groupSave($formId, $data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::GROUPSAVE_API;
+        $postData = [
+            'data' => [
+                'formid' => $formId,
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 拆单
+     * @param $formId
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function disassembly($formId, $data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::DISASSEMBLY_API;
+        $postData = [
+            'data' => [
+                'formid' => $formId,
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 工作流审批
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function workflowAudit($data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::WORKFLOWAUDIT_API;
+        $postData = [
+            'data' => [
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 查询分组信息
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function queryGroupInfo($data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::QUERYGROUPINFO_API;
+        $postData = [
+            'data' => [
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
+
+    /**
+     * 分组删除
+     * @param $data
+     * @param string $format
+     * @return mixed|string|void
+     */
+    public function groupDelete($data, string $format = 'string')
+    {
+        $url = $this->cloudUrl . ApiPathConst::GROUPDELETE_API;
+        $postData = [
+            'data' => [
+                'data' => $data
+            ]
+        ];
+        return $this->webApiClient->execute($url, [], $postData, $format);
+    }
 }
